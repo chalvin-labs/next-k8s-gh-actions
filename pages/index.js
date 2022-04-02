f5462a7cae8c90d608bc271df486290a2ca35701
Chalvin Wiradhika <chalvinwz@gmail.com>
2022-04-02 19:23:03 +0700

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+	const [data, setData] = useState([])
+
+	const fetchData = async () => {
+		const res = await axios.get('http://localhost')
+		setData(res)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	console.log(data)
+
 	return (
-		<div>
-			<h1>Test</h1>
+		<div className='flex justify-center'>
+			<div className='w-96 flex justify-center'>
+				<h1 className='text-4xl font-bold'>{data.name}</h1>
+			</div>
 		</div>
 	)
 }
